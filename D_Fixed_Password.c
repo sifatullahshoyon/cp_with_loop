fc7d6e00c8daa7e552a1dcc53a83f068e9391db7
freelancersifat627e <freelancersifat627@gmail.comm>
2025-10-16 01:39:00 +0600

    EOF

---
+++ D_Fixed_Password.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+    int x;
+    // EOF = End Of File
+    while(scanf("%d" , &x) != EOF){
+
+        if(x == 1999){
+            printf("Correct\n");
+            break;
+        }else{
+            printf("Wrong\n");
+        }
+    }
+    
+    
+    return 0;
+}
